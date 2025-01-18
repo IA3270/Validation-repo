a54592d3670b6cb74e0cfd32c8693e8959bff25e
root <root@devops.Home>
2025-01-18 12:32:01 +0000

    Updating my code

--- Script1.sh
+++ Script1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Task 1 complete!!" "This is a further test" "This additional 'code' fires off Project 2!" "Additional Test!"
-echo "Additional Test on Jenkins rebuild!"
+echo "Additional Test on Jenkins rebuild!" - "Final Trigger Test"
